c981b09866b4740298860eb7cf97dcb9af5da030
Aurynth <55421570+Aurynth@users.noreply.github.com>
2021-01-09 14:16:01 -0600

    spawn enemies
    spawns enemies from top of screen

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float speed = 10.0f;
+    private Rigidbody2D rb;
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -screenBounds.y * 1.5) Destroy(this.gameObject);
+    }
+}
