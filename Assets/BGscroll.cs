7190265db82a36f8b5b9c1b4d7609871b4cadab8
Aurynth <55421570+Aurynth@users.noreply.github.com>
2021-01-09 10:55:12 -0600

    Background scrolling
    Have an infinite scrolling background

---
+++ Assets/BGscroll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class BGscroll : MonoBehaviour
+{
+    private float length, startpos, time;
+    public GameObject cam;
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += 1;
+        float temp = (cam.transform.position.x * (1 - speed));
+        float dist = (startpos - (time * speed));
+
+        transform.position = new Vector3(dist, transform.position.y, transform.position.z);
+        if (dist < -1 * length) startpos += 2 * length;
+        //else if (temp < startpos - length) startpos -= length;
+    }
+}
